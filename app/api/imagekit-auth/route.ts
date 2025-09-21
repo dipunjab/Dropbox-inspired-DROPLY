26dae983c5aa33c42af0fddbb867cb0d8593e68b
Usman <ghaniusman0287@gmail.com>
2025-09-21 12:53:26 +0500

    Add ImageKit integration for image uploads and authentication

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import  ImageKit from "imagekit";
+
+
+const imagekit = new ImageKit({
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || ""
+})
+
+
+export async function GET() {
+    try {
+        const userId = await auth();
+        if (!userId) {
+            return NextResponse.json({error: "Unauthorized"}, {status: 401})
+        }
+    
+        const authParams = imagekit.getAuthenticationParameters();
+    
+        return NextResponse.json(authParams)
+    } catch (error) {
+        console.log("ImageKitauth Error", error);
+        
+        return NextResponse.json({error: "failed to generate authentication parameters for imagekit"}, {status: 500})
+    }
+}
