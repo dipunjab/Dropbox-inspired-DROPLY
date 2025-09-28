92bd4710ece222396848237396ca7eb669050fa8
Usman <ghaniusman0287@gmail.com>
2025-09-28 10:09:26 +0500

    Add Providers component for ImageKit and theme management; create sign-in and sign-up pages

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from '@/components/SigninForm'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div>
+      <SignInForm/>
+    </div>
+  )
+}
+
+export default SignInPage
